3b2c8583a5d6f4524b7527bda533fa06879519b4
IlyaChef <ilyaromanov89@gmail.com>
2023-02-23 19:56:19 +0300

    parametrize added

--- tests/test_two_date_parser.py
+++ tests/test_two_date_parser.py
@@ -1,5 +1,16 @@
+import datetime
+import pytest
 from functions.two_date_parser import compose_datetime_from
 
 
-def test_compose_datetime_from():
-    pass
+@pytest.mark.parametrize(
+    'date_str, time_str, expected_result',
+    [
+    ('today', '22:55', '2023-02-23 22:55:00'),
+    ('tomorrow', '22:55', '2023-02-24 22:55:00'),
+    ],
+)
+
+def test_compose_datetime_from(date_str, time_str, expected_result):
+    assert str(compose_datetime_from(date_str, time_str)) == expected_result
+
