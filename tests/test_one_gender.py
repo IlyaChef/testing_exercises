28e4afb8c41f64a061151e19d158116311b0fa23
IlyaChef <ilyaromanov89@gmail.com>
2023-02-23 10:34:16 +0300

    parametrize test

--- tests/test_one_gender.py
+++ tests/test_one_gender.py
@@ -1,7 +1,16 @@
+import pytest
 from functions.one_gender import genderalize
 
+@pytest.mark.parametrize(
+    'verb_male, verb_female, gender, expected_result',
+    [
+        ('verb_male', 'verb_female', 'male', 'verb_male'),
+        ('verb_male', 'verb_female', 'female', 'verb_female'),
+    ],
+)
 
-def test_genderalize() -> str:
-    assert genderalize('сделал', 'сделала', 'male') == 'сделал'
-    assert genderalize('унес', 'унесла', 'female') == 'унесла'
+
+def test_genderalize(verb_male, verb_female, gender, expected_result):
+    assert genderalize(verb_male, verb_female, gender) == expected_result
+
 
