9e946e892c4bf663190d6b61bb8ae6fe9d009f08
IlyaChef <ilyaromanov89@gmail.com>
2023-02-25 01:21:33 +0300

    test three added

--- tests/conftest.py
+++ tests/conftest.py
@@ -10,3 +10,18 @@ def verb_male():
 @pytest.fixture
 def verb_female():
     return 'накодила'
+
+
+@pytest.fixture
+def host_name():
+    return 'https://www.avito.ru'
+
+
+@pytest.fixture
+def relative_url():
+    return 'moskva_i_mo/nedvizhimost'
+
+
+@pytest.fixture
+def get_params():
+    return {'localPriority': '0', 'q': 'дом'}
--- tests/test_three_url_builder.py
+++ tests/test_three_url_builder.py
@@ -1,5 +1,13 @@
+import pytest
+from typing import Mapping
 from functions.three_url_builder import build_url
 
+@pytest.mark.parametrize(
+    'host_name, relative_url, get_params, expected_result',
+    [
+        ('https://www.avito.ru', 'moskva_i_mo/nedvizhimost', {'localPriority': '0', 'q': 'дом'}, 'https://www.avito.ru/moskva_i_mo/nedvizhimost?localPriority=0&q=дом'),
+    ],
+)
 
-def test_build_url():
-    pass
+def test_build_url(host_name, relative_url, get_params: Mapping[str, str], expected_result: str):
+    assert build_url(host_name, relative_url, get_params) == expected_result
